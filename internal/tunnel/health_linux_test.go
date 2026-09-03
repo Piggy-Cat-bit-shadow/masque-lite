402c3c64484a4258a7aad9d6a1baa0320beaa611
Piggy-Cat-bit-shadow <185194848+Piggy-Cat-bit-shadow@users.noreply.github.com>
2026-09-03 10:54:05 +0800

    Fix sanitized interface test fixtures

--- internal/tunnel/health_linux_test.go
+++ internal/tunnel/health_linux_test.go
@@ -9,17 +9,17 @@ import (
 )
 
 func TestNormalizeInterfacePrefix(t *testing.T) {
-	mapped := net.IP{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0xff, 10, 89, 0, 1}
+	mapped := net.IP{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0xff, 192, 0, 2, 1}
 	tests := []struct {
 		name string
 		addr net.Addr
 		want netip.Prefix
 		ok   bool
 	}{
-		{"ipv4", &net.IPNet{IP: net.IPv4(10, 89, 0, 1).To4(), Mask: net.CIDRMask(16, 32)}, netip.MustParsePrefix("192.0.2.1/24"), true},
-		{"mapped-ipv4", &net.IPNet{IP: mapped, Mask: net.CIDRMask(16, 32)}, netip.MustParsePrefix("192.0.2.1/24"), true},
+		{"ipv4", &net.IPNet{IP: net.IPv4(192, 0, 2, 1).To4(), Mask: net.CIDRMask(24, 32)}, netip.MustParsePrefix("192.0.2.1/24"), true},
+		{"mapped-ipv4", &net.IPNet{IP: mapped, Mask: net.CIDRMask(24, 32)}, netip.MustParsePrefix("192.0.2.1/24"), true},
 		{"ipv6", &net.IPNet{IP: net.ParseIP("fe80::1"), Mask: net.CIDRMask(64, 128)}, netip.MustParsePrefix("fe80::1/64"), true},
-		{"invalid-mask", &net.IPNet{IP: net.IPv4(10, 89, 0, 1).To4(), Mask: net.IPMask{255, 0}}, netip.Prefix{}, false},
+		{"invalid-mask", &net.IPNet{IP: net.IPv4(192, 0, 2, 1).To4(), Mask: net.IPMask{255, 0}}, netip.Prefix{}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -37,8 +37,8 @@ func TestNormalizedPrefixDoesNotMatchWrongIPv4(t *testing.T) {
 		addr net.Addr
 		want netip.Prefix
 	}{
-		{"different-address", &net.IPNet{IP: net.IPv4(10, 89, 0, 2), Mask: net.CIDRMask(16, 32)}, netip.MustParsePrefix("192.0.2.1/24")},
-		{"different-prefix-length", &net.IPNet{IP: net.IPv4(10, 89, 0, 1), Mask: net.CIDRMask(24, 32)}, netip.MustParsePrefix("192.0.2.1/24")},
+		{"different-address", &net.IPNet{IP: net.IPv4(192, 0, 2, 2), Mask: net.CIDRMask(24, 32)}, netip.MustParsePrefix("192.0.2.1/24")},
+		{"different-prefix-length", &net.IPNet{IP: net.IPv4(192, 0, 2, 1), Mask: net.CIDRMask(30, 32)}, netip.MustParsePrefix("192.0.2.1/24")},
 	} {
 		t.Run(tt.name, func(t *testing.T) {
 			got, ok := normalizeInterfacePrefix(tt.addr)
